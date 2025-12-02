1950088bc0b1dfae8bee01caaaab5a94694f6aa6
Aakash Rayamajhi <aakashrayamajhi03@gmail.com>
2025-12-02 09:04:32 +0545

    express

--- express/index.js
+++ express/index.js
@@ -1,13 +1,15 @@
 import express from 'express'
-
 const app = express()
-const port = 3000
 
-app.use((req, res) => {
-
-
+app.get('/', (req, res) => {
+    res.send('home page')
+})
+app.get('/about', (req, res) => {
+    res.send('about page')
 })
 
+
+const port = 3000
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
 })
---
+++ express/mod.js
@@ -0,0 +1,5 @@
+const exp = () => {
+    console.log("This is express mod");
+}
+
+export default exp
